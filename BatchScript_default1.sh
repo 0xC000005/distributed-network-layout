7c8a1717b242cd92bc5dbe18fc3391fb70447c5f
Max Jingwei Zhang <maxjingwei.zhang@ryerson.ca>
2022-09-17 19:31:59 -0400

    update

--- BatchScript_default1.sh
+++ BatchScript_default1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=20
-#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=15
 #SBATCH --time=23:59:59
 #SBATCH --account=rrg-primath
@@ -46,7 +46,7 @@ echo "SLURM_CPUS_PER_TASK = "$SLURM_CPUS_PER_TASK
 #start worker nodes
 NWORKERS=$((SLURM_NNODES - 1))
 
-SPARK_NO_DAEMONIZE=1 srun -n 95 -N ${NWORKERS} -r 1 --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m ${SLURM_SPARK_MEM}M -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} & slaves_pid=$!
+SPARK_NO_DAEMONIZE=1 srun -n {NWORKERS} -N ${NWORKERS} -r 1 --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m ${SLURM_SPARK_MEM}M -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} & slaves_pid=$!
 
 srun -n 1 -N 1 spark-submit --master ${MASTER_URL} --driver-memory 180g --executor-memory ${SLURM_SPARK_MEM}M --conf spark.driver.maxResultSize=2g --conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.dynamicAllocation.enabled=False --conf spark.scheduler.listenerbus.eventqueue.capacity=20000 --conf "spark.executor.extraJavaOptions= -XX:+UseG1GC -XX:+UnlockDiagnosticVMOptions -XX:+G1SummarizeConcMark -XX:InitiatingHeapOccupancyPercent=35 -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:OnOutOfMemoryError='kill -9 %p' -Xloggc:$SCRATCH/log/ -XX:+UseCompressedOops" --conf "spark.driver.extraJavaOptions=-XX:+UseG1GC -XX:+UnlockDiagnosticVMOptions -XX:+G1SummarizeConcMark -XX:InitiatingHeapOccupancyPercent=35 -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:OnOutOfMemoryError='kill -9 %p' -Xloggc:$SCRATCH/log/ -XX:+UseCompressedOops" --packages graphframes:graphframes:0.8.0-spark2.4-s_2.11 --repositories https://repos.spark-packages.org DistributedLayoutAlgorithm.py SNAP/com-friendster.ungraph.txt output/ 100
 
