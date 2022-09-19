dad6757aadd55ce63f2e7dee55322e8faf09a36e
Max Zhang <0xc00005@gmail.com>
2022-09-19 12:09:55 -0400

    add test to smaller dataset

--- BatchScript_default2.sh
+++ BatchScript_default2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=20
-#SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=10
-#SBATCH --time=0:45:0
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=15
+#SBATCH --time=23:59:59
 #SBATCH --account=rrg-primath
 #SBATCH --job-name=DistributedLayoutAlgorithm
 #SBATCH --output=jobOutput/DistributedLayoutAlgorithm_output_%j.txt
@@ -28,13 +28,15 @@ export SPARK_IDENT_STRING=$SLURM_JOBID
 export SPARK_WORKER_DIR=$SLURM_TMPDIR
 export SLURM_SPARK_MEM=$(printf "%.0f" $((${SLURM_MEM_PER_NODE} *95/100)))
 export PYTHONPATH=$PYTHONPATH:/cvmfs/soft.computecanada.ca/easybuild/software/2017/Core/spark/2.4.4/python/lib/py4j-0.10.7-src.zip
-export _JAVA_OPTIONS="-Xms256m -Xmx20g"
-
 start-master.sh
+
 sleep 5
 #get URL of master node
 MASTER_URL=spark://$(scontrol show hostname $SLURM_NODELIST | head -n 1):7077
+# MASTER_URL=$(grep -Po '(?=spark://).*' $SPARK_LOG_DIR/spark-${SPARK_IDENT_STRING}-org.apache.spark.deploy.master*.out)
+
 NWORKERS=$((SLURM_NNODES - 1))
+
 echo "Master URL = "$MASTER_URL
 echo "Number of Workers = "$NWORKERS
 echo "SPARK_IDENT_STRING = "$SPARK_IDENT_STRING
@@ -42,15 +44,13 @@ echo "SPARK_WORKER_DIR = "$SPARK_WORKER_DIR
 echo "SLURM_MEM_PER_NODE = "$SLURM_MEM_PER_NODE
 echo "SLURM_SPARK_MEM= "$SLURM_SPARK_MEM
 echo "SLURM_CPUS_PER_TASK = "$SLURM_CPUS_PER_TASK
-
+echo "NWORKERS = "$NWORKERS
 
 #start worker nodes
-SPARK_NO_DAEMONIZE=1 srun -n 152 -N ${NWORKERS} -r 1 --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m 20g -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} & slaves_pid=$!
 
-# srun -n 1 -N 1 spark-submit --master ${MASTER_URL} --driver-memory 180g --conf spark.driver.memoryOvehead=18g --executor-memory 180g --conf spark.executor.memoryOverhead=18g --conf spark.memory.fraction=0.2 --conf spark.driver.maxResultSize=0 --conf spark.memory.storageFraction=0.5 --conf spark.default.parallelism=9120 --conf spark.sql.shuffle.partitions=9120 --conf spark.shuffle.io.maxRetries=10 --conf spark.blacklist.enabled=False  --conf spark.shuffle.io.retryWait=60s --conf spark.reducer.maxReqsInFlight=1 --conf spark.shuffle.io.backLog=4096 --conf spark.network.timeout=1200 --conf "spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:$SCRATCH/log/ -XX:+UseCompressedOops" --conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.scheduler.listenerbus.eventqueue.capacity=20000 --packages graphframes:graphframes:0.8.0-spark2.4-s_2.11  --repositories https://repos.spark-packages.org DistributedLayoutAlgorithm.py SNAP/com-friendster.ungraph.txt output/ 100
-# srun -n 1 -N 1 spark-submit --master ${MASTER_URL} -XX:+PrintGCTimeStamps -Xloggc:$SCRATCH/log/ -XX:+UseCompressedOops --packages graphframes:graphframes:0.8.0-spark2.4-s_2.11  --repositories https://repos.spark-packages.org DistributedLayoutAlgorithm.py sampleInput1/covid19vaccine_twitter_replyto_net_feb2021.tsv output/ 100
+SPARK_NO_DAEMONIZE=1 srun -n ${NWORKERS} -N ${NWORKERS} -r 1 --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m ${SLURM_SPARK_MEM}M -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} & slaves_pid=$!
 
-srun -n 1 -N 1 spark-submit --master ${MASTER_URL} spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:$SCRATCH/log/ -XX:+UseCompressedOops --conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.scheduler.listenerbus.eventqueue.capacity=20000 --packages graphframes:graphframes:0.8.0-spark2.4-s_2.11  --repositories https://repos.spark-packages.org DistributedLayoutAlgorithm.py sampleInput1/covid19vaccine_twitter_replyto_net_feb2021.tsv output/ 100
+srun -n 1 -N 1 spark-submit --master ${MASTER_URL} --driver-memory 180g --executor-memory ${SLURM_SPARK_MEM}M --conf spark.driver.maxResultSize=2g --conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.dynamicAllocation.enabled=False --conf spark.scheduler.listenerbus.eventqueue.capacity=20000 --conf "spark.executor.extraJavaOptions= -XX:+UseG1GC -XX:+UnlockDiagnosticVMOptions -XX:+G1SummarizeConcMark -XX:InitiatingHeapOccupancyPercent=35 -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:OnOutOfMemoryError='kill -9 %p' -Xloggc:$SCRATCH/log/ -XX:+UseCompressedOops" --conf "spark.driver.extraJavaOptions=-XX:+UseG1GC -XX:+UnlockDiagnosticVMOptions -XX:+G1SummarizeConcMark -XX:InitiatingHeapOccupancyPercent=35 -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:OnOutOfMemoryError='kill -9 %p' -Xloggc:$SCRATCH/log/ -XX:+UseCompressedOops" --packages graphframes:graphframes:0.8.0-spark2.4-s_2.11 --repositories https://repos.spark-packages.org DistributedLayoutAlgorithm.py sampleInput1/covid19vaccine_twitter_replyto_net_feb2021.tsv output/ 100
 
 kill $slaves_pid
 stop-master.sh
